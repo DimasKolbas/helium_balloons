cf9adfcc9caef85fb5cafe5e13c481e0f16278ea
Dmitriy Yermak <d707225@gmail.com>
2023-09-10 22:25:49 +0300

    FAQ section for mac

--- js/click.js
+++ js/click.js
@@ -1,15 +1,32 @@
-// Получаем ссылки на элементы
-const toggleButton = document.getElementById("toggleButton");
-const hiddenField = document.getElementById("hiddenField");
+// Получите все элементы вопросов и ответов
+const questionItems = document.querySelectorAll('.faq__questions-item');
 
-// Добавляем обработчик события клика
-toggleButton.addEventListener("click", function () {
-    // При клике изменяем свойства скрытого поля
-    if (hiddenField.style.opacity === "1") {
-        hiddenField.style.opacity = "0";
-        hiddenField.style.display = "none";
-    } else {
-        hiddenField.style.opacity = "1";
-        hiddenField.style.display = "block";
-    }
+// Добавьте обработчик клика к каждому элементу вопроса
+questionItems.forEach((item) => {
+    item.addEventListener('click', (event) => {
+        // Проверяем, есть ли у текущего вопроса класс "active"
+        const isActive = item.classList.contains('active');
+
+        // Удалите класс "active" у всех вопросов
+        questionItems.forEach((otherItem) => {
+            otherItem.classList.remove('active');
+        });
+
+        // Если текущий вопрос не активен (не открыт), то добавляем класс "active"
+        if (!isActive) {
+            item.classList.add('active');
+        }
+
+        // Остановите всплытие события, чтобы клик на вопросе не закрывал ответ
+        event.stopPropagation();
+    });
 });
+
+// Добавьте обработчик клика к документу
+document.addEventListener('click', () => {
+    // Закройте все открытые ответы
+    questionItems.forEach((item) => {
+        item.classList.remove('active');
+    });
+});
+
